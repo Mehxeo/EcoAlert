63ae45238861d6d034ff79c9c769d52e34943dbf
Andrew Hu <andrewhu32009@gmail.com>
2025-04-06 04:56:23 -0400

    ecopoints frontend and fade ins

--- js/ecopoints.js
+++ js/ecopoints.js
@@ -1,2 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+    function animateContainers(element, newValue) {
+        element.classList.remove('fade-in');
+        element.classList.add('fade-out');
+
+        setTimeout(() => {
+            element.innerHTML = newValue;
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        }
+        , 500);
+    }
 });
